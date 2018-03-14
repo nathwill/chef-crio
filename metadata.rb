8856df62ef640531e59d46ac3cf810f4a7d96e34
Nathan Williams <nath.e.will@gmail.com>
2018-03-13 17:25:29 -0700

    fix os ver spec

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures crio'
 version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-supports 'centos', '>= 7'
+supports 'centos', '>= 7.0'
 
 issues_url 'https://github.com/nathwill/chef-crio/issues'
 source_url 'https://github.com/nathwill/chef-crio'
