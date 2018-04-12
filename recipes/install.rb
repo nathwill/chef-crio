30d2c57b078b207c58e252428a5634b0a2e0fbf2
Nathan Williams <nath.e.will@gmail.com>
2018-04-12 13:32:46 -0700

    support alternate package source

--- recipes/install.rb
+++ recipes/install.rb
@@ -9,6 +9,15 @@ yum_repository 'virt7-container-common' do
   description 'Container Virtualization Extras for EL'
   baseurl 'https://cbs.centos.org/repos/virt7-container-common-candidate/x86_64/os/'
   gpgcheck false
+  not_if { node['crio']['repo'] == 'upstream-crio-family' }
+  only_if { platform?('centos') }
+end
+
+yum_repository 'upstream-crio-family' do
+  description 'Copr repo for Upstream_CRIO_Family owned by baude'
+  baseurl 'https://copr-be.cloud.fedoraproject.org/results/baude/Upstream_CRIO_Family/epel-7-$basearch/'
+  gpgkey 'https://copr-be.cloud.fedoraproject.org/results/baude/Upstream_CRIO_Family/pubkey.gpg'
+  not_if { node['crio']['repo'] == 'virt7-container-common' }
   only_if { platform?('centos') }
 end
 
