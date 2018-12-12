6f47c224d6870af9e276e8f69119f358b91a0b37
Nathan Williams <nath.e.will@gmail.com>
2018-12-11 19:07:06 -0800

    bump ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'nath.e.will@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures crio'
 long_description 'Installs/Configures crio'
-version '1.3.2'
+version '1.3.3'
+
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 supports 'centos', '>= 7.0'
